5aa559a845751d82790cdfb92844240b3740cc90
Codemancer <codemancer.io@gmail.com>
2024-09-22 16:03:06 +0700

    Type conversion

--- types.go
+++ types.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/cmplx"
-)
-
-var (
-	goIsFun bool       = true
-	maxInt  uint64     = 1<<64 - 1
-	complex complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	const f = "%T(%v)\n"
-	fmt.Printf(f, goIsFun, goIsFun)
-	fmt.Printf(f, maxInt, maxInt)
-	fmt.Printf(f, complex, complex)
+	i := 42
+	f := float64(i)
+	u := uint(f)
+
+	fmt.Println(i, f, u)
 }

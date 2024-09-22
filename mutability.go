5a3ce12d44c2138646bd8a0f1548be1aea74ffdb
Codemancer <codemancer.io@gmail.com>
2024-09-22 15:57:19 +0700

    Function: pass by values

---
+++ mutability.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Artist struct {
+	Name, Genre string
+	Songs       int
+}
+
+func newReleasae(a Artist) int {
+	a.Songs++
+	return a.Songs
+}
+
+func main() {
+	me := Artist{Name: "Matt", Genre: "Electro", Songs: 42}
+	fmt.Printf("%s released their %dth song\n", me.Name, newReleasae(me))
+	fmt.Printf("%s has a total of %d songs", me.Name, me.Songs)
+}

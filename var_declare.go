cacc375a748085ff1de1e6181b44fa2804273b64
Codemancer <codemancer.io@gmail.com>
2024-09-22 15:28:20 +0700

    declared var type 1

---
+++ var_declare.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var (
+	name string
+	age int
+	location string
+)
+
+func main() {
+	name = "pat"
+	age = 43
+	location = "Bangkok"
+	fmt.Println(name, age, location)
+}

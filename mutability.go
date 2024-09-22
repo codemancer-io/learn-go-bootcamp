1db192c2313ae775d8d6e91f6ebbc657ecbc5392
Codemancer <codemancer.io@gmail.com>
2024-09-22 15:58:33 +0700

    Function: pass by reference

--- mutability.go
+++ mutability.go
@@ -7,13 +7,13 @@ type Artist struct {
 	Songs       int
 }
 
-func newReleasae(a Artist) int {
+func newReleasae(a *Artist) int {
 	a.Songs++
 	return a.Songs
 }
 
 func main() {
-	me := Artist{Name: "Matt", Genre: "Electro", Songs: 42}
+	me := &Artist{Name: "Matt", Genre: "Electro", Songs: 42}
 	fmt.Printf("%s released their %dth song\n", me.Name, newReleasae(me))
 	fmt.Printf("%s has a total of %d songs", me.Name, me.Songs)
 }
